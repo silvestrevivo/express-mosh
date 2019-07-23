57ea9e73cc338d5e582c03ad2a7cc8fa8a08478c
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-24 00:09:54 +0200

    get and post request with dummy data

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+  {id: 1, name: 'course1'},
+  {id: 2, name: 'course2'},
+  {id: 3, name: 'course3'}
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  // 404 => object not found, not exists in the server
+  if(!course) res.status(404).send('The course with the given ID was not found');
+  res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+
+  courses.push(course);
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
