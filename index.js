328a61e8c6e187cd53f7adda90e29276863822d6
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-24 08:50:55 +0200

    input validation with Joi package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
 
 app.use(express.json());
@@ -26,6 +27,15 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  }
+  const result = Joi.validate(req.body, schema);
+  if(result.error){
+    // in post request, 400 means a BAD request
+    res.status(400).send(result.error.details[0].message);
+    return
+  }
   const course = {
     id: courses.length + 1,
     name: req.body.name
